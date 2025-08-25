b9a275e04a34b251dc3bb79c731363054a536af7
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-25 20:55:06 +0500

    add serializers

---
+++ users/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from .models import CustomUser, phone_regex
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'phone_number', 'email', 'role']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'phone_number', 'email', 'role', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=13, phone_regex=[phone_regex])
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        phone_number = attrs.get('phone_number')
+        password = attrs.get('password')
+
+        user = authenticate(phone_number=phone_number, password=password)
+        if not user:
+            raise serializers.ValidationError('Invalid credentials')
+
+        attrs['user'] = user
+        return attrs
