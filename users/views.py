fc033217d60a323b5b09cf02bf7c6336d92bcda0
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-26 10:23:30 +0500

    add views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
+from .serializers import LoginSerializer, RegisterSerializer, UserSerializer
+from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status, generics
 
-# Create your views here.
+
+class LoginView(APIView):
+    @swagger_auto_schema(request_body=LoginSerializer)
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh_token = RefreshToken.for_user(user)
+            access_token = refresh_token.access_token
+            return Response({
+                'refresh': str(refresh_token),
+                'access': str(access_token)
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(APIView):
+    @swagger_auto_schema(request_body=RegisterSerializer)
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh_token = RefreshToken.for_user(user)
+            access_token = refresh_token.access_token
+            return Response({
+                'refresh': str(refresh_token),
+                'access': str(access_token)
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
