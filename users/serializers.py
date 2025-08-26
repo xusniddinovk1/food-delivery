1ee02a82bd5ab317f628b2b4926f595c39c125c2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-26 10:24:10 +0500

    add method to serializer

--- users/serializers.py
+++ users/serializers.py
@@ -8,6 +8,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'phone_number', 'email', 'role']
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
