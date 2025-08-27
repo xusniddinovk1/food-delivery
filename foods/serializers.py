09af6337b905608493fb2d931017b1b4a4b4913c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-27 10:45:07 +0500

    add serializers

---
+++ foods/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Restaurant, Category, Food
+
+
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ['id', 'title', 'description', 'price', 'is_available', 'prep_to_min', 'category', 'restaurant']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    foods = FoodSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'restaurant', 'foods']
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True, read_only=True)
+    foods = FoodSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Restaurant
+        fields = ["id", "name", "address", "latitude", "longitude", "is_open", "categories", "foods"]
