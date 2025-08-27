1a2e16a288aff08bea11635178b8da4a414540bd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-27 10:54:15 +0500

    add viewsets

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import RestaurantSerializer, CategorySerializer, FoodSerializer
+from .models import Restaurant, Category, Food
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.filter(is_open=True)
+    serializer_class = RestaurantSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny]
+        return [IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(restaurant=self.request.user.restaurant)
+
+    def perform_create(self, serializer):
+        serializer.save(restaurant=self.request.user.restaurant)
+
+
+class FoodViewSet(viewsets.ModelViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Food.objects.filter(restaurant=self.request.user.restaurant)
+
+    def perform_create(self, serializer):
+        serializer.save(restaurant=self.request.user.restaurant)
