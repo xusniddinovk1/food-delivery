6d9c98d50dcc75cc5db43f60c87bc3fa70c5719d
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-12-06 16:59:10 +0500

    crate views and urls

--- foods/urls.py
+++ foods/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from .views import RestaurantViewSet, CategoryViewSet, FoodViewSet, PublicRestaurantViewSet, PublicCategoryViewSet, \
     PublicFoodViewSet
 from rest_framework.routers import DefaultRouter
+from django.urls import path
+from orders.views import (
+    CartView, RemoveFromCartView,
+    CreateOrderView, OrderListView, OrderDetailView
+)
 
 router = DefaultRouter()
 router.register(f'restaurant', PublicRestaurantViewSet, basename='restaurants')
@@ -13,5 +18,13 @@ router.register(r'my-categories', CategoryViewSet, basename='my-categories')
 router.register(r'my-foods', FoodViewSet, basename='my-foods')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+]
+
+urlpatterns += [
+    path("cart/", CartView.as_view()),
+    path("cart/remove/", RemoveFromCartView.as_view()),
+    path("order/create/", CreateOrderView.as_view()),
+    path("orders/", OrderListView.as_view()),
+    path("orders/<int:pk>/", OrderDetailView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from .models import Cart, CartItem, OrderItem, Order
+from .serializers import CartSerializer, OrderSerializer
+from foods.models import Food
+from rest_framework.generics import DestroyAPIView, GenericAPIView, ListAPIView, RetrieveAPIView
 
-# Create your views here.
+
+class CartView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        cart, _ = Cart.objects.get_or_create(customer=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+
+    def post(self, request):
+        cart, _ = Cart.objects.get_or_create(customer=request.user)
+        food_id = request.data.get('food')
+        quantity = int(request.data.get('quantity', 1))
+
+        try:
+            food = Food.objects.get(id=food_id)
+        except Food.DoesNotExist:
+            return Response({
+                'error': 'Food not found'
+            }, status=404)
+
+        item, created = CartItem.objects.get_or_create(cart=cart, food=food)
+
+        if created:
+            item.quantity = quantity
+        else:
+            item.quantity += quantity
+
+        item.save()
+        return Response({"message": "Added to cart"})
+
+
+class RemoveFromCartView(DestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        cart = Cart.objects.get(customer=request.user)
+        food_id = request.data.get("food")
+
+        try:
+            item = CartItem.objects.get(cart=cart, food_id=food_id)
+        except CartItem.DoesNotExist:
+            return Response({"error": "Item not found"}, status=404)
+
+        item.delete()
+        return Response({"message": "Removed from cart"})
+
+
+class CreateOrderView(GenericAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        cart = Cart.objects.get(customer=request.user)
+
+        if cart.items.count() == 0:
+            return Response({"error": "Cart is empty"}, status=400)
+
+        restaurant = cart.items.first().food.restaurant
+
+        order = Order.objects.create(
+            customer=request.user,
+            phone_number=request.user.phone_number,
+            restaurant=restaurant
+        )
+
+        for item in cart.items.all():
+            OrderItem.objects.create(
+                order=order,
+                food=item.food,
+                quantity=item.quantity,
+                price=item.food.price
+            )
+
+        cart.items.all().delete()
+
+        return Response(OrderSerializer(order).data, status=201)
+
+
+class OrderListView(ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(customer=self.request.user)
+
+
+class OrderDetailView(RetrieveAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(customer=self.request.user)
